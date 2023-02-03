818f95b4ef29daa79205564fa1c0571f96f45322
KariGM <karengarmed@gmail.com>
2023-02-03 10:43:30 -0800

    chore: adds the basic store files

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -3,11 +3,14 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { Box } from "native-base";
+
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <Box>Hello world</Box>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
