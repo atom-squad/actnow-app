e4b8e677392c4137e6db588c20a11b4009fc69ac
Tanya <ttanya01@mylangara.ca>
2023-02-27 23:01:10 -0800

    chore: Signup component revised

---
+++ components/UsernameWithRank.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Box, Flex, Text } from "native-base";
+
+const UsernameWithRank = ({bgcolor, textcolor, rank, username, points}) => {
+    return (
+        <Box backgroundColor={bgcolor} style={styles.container}>
+          <Flex direction='row' justifyContent="space-around">
+            <Text color={textcolor} bold>#{rank}</Text>
+            <Text color={textcolor}>{username}</Text>
+            <Text color={textcolor} bold>{points}</Text>
+          </Flex>
+        </Box>
+    );
+  }
+
+  const styles = {
+    container: {
+        paddingTop: 14,
+        paddingBottom: 14,
+        borderWidth: 1,
+        borderColor: "black",
+        borderTopWidth: 0
+    }
+  }
+
+  export default UsernameWithRank;
