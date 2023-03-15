580adbe3e3b6781450bd071287ea1dad7c0a6094
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-03-15 16:27:46 -0700

    CAR-137 Update Week Calendar Component with react-native-calendar-strip package

--- components/WeekCalendar.tsx
+++ components/WeekCalendar.tsx
@@ -1,53 +1,28 @@
+import { View } from 'native-base';
 import React from 'react';
-import { Text, View } from 'native-base';
+import CalendarStrip  from 'react-native-calendar-strip';
 
 const WeekCalendar = () => {
-  // an array of 7 days starting from today
-  const days = [];
-  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const today = new Date();
-  for (let i = 0; i < 7; i++) {
-    const day = new Date(today);
-    day.setDate(today.getDate() + i);
-    days.push({
-      date: day.getDate(),
-      dayOfWeek: daysOfWeek[day.getDay()],
-    });
-  }
-
   return (
-    <View style={styles.week as any}>
-      {days.map((day, index) => (
-        <View style={styles.day as any} key={index}>
-          <Text style={styles.date as any}>{day.date}</Text>
-          <Text style={styles.dayOfWeek as any}>{day.dayOfWeek}</Text>
-        </View>
-      ))}
-    </View>
-  );
+    <CalendarStrip
+    style={{
+          height: 80, 
+          marginTop: 30,
+          paddingTop: 10, 
+          paddingBottom: 10, 
+          borderWidth: 0.5, 
+          width: '90%', 
+          alignSelf: 'center', 
+          borderRadius: 12,
+          backgroundColor: 'white',
+          borderColor: '#DBE1E3'
+          }}
+      iconStyle={{display: 'none'}}
+      calendarHeaderStyle={{display: 'none'}}
+      innerStyle={{padding: 5, paddingBottom: 5}}
+
+    />
+  )
 };
 
-const styles = {
-  week: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-  },
-  day: {
-    alignItems: 'center',
-  },
-  dayOfWeek: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  date: {
-    fontSize: 14,
-    fontWeight: '400',
-  },
-};
-
-export default WeekCalendar;
+export default WeekCalendar
