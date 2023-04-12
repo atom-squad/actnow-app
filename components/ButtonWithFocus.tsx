b74f44f9155bb380567a8d1d9e4f09066c225f9f
luferupa <luferupa@gmail.com>
2023-04-11 19:08:39 -0700

    fix: Fixing actions cards size, selected values for pressables, app details

--- components/ButtonWithFocus.tsx
+++ components/ButtonWithFocus.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Pressable, Text } from "native-base";
 import { COLORS } from "../common/constants";
 
-const ButtonWithFocus = ({title, style, onClickAction}) => {
+const ButtonWithFocus = ({title, style, onClickAction, selected}) => {
     return (
-      <Pressable style={style} borderBottomColor={COLORS.gray2} borderBottomWidth={1} paddingBottom={2} marginBottom={2} 
-         _hover={{ borderBottomColor: COLORS.greenPrimary, borderBottomRadius: 2 }}
+      <Pressable style={style} borderBottomColor={selected? COLORS.greenPrimary : COLORS.gray2} 
+      borderBottomWidth={2} paddingBottom={2} marginBottom={2} 
         _pressed={{ borderBottomColor: COLORS.greenPrimary, borderBottomRadius: 2 }} 
         _focus={{borderBottomColor: COLORS.greenPrimary, borderBottomRadius: 2 }}
         onPress={() => onClickAction(title)}
@@ -19,7 +19,7 @@ const ButtonWithFocus = ({title, style, onClickAction}) => {
             alignSelf="center" 
             fontFamily="albert-semibold" 
             fontSize={18}
-            color={isPressed ? COLORS.greenPrimary : isHovered ? COLORS.greenPrimary : isFocused ? COLORS.greenPrimary  : COLORS.gray2}  
+            color={isPressed ? COLORS.greenPrimary : selected? COLORS.greenPrimary : isFocused ? COLORS.greenPrimary  : COLORS.gray2}  
           >{title}</Text>
           }}
       </Pressable>
